dacf5e9afc8c74cefa2ef17ceecda21160046f31
Tobias Pohl <119701302+topohl@users.noreply.github.com>
2024-10-24 15:56:34 +0200

    Update CookieHab.R
    + refined analysis
    + improved installing and loading of libraries

--- Analysis/CookieHab.R
+++ Analysis/CookieHab.R
@@ -1,119 +1,143 @@
 ################################################################################
 ## Create the cookie plot
 ## Compare Means between Groups during 2 hours preceding the Active Phase
-#  This is used to quantify activity before the onset of the active phase
+# This is used to quantify activity before the onset of the active phase
 ################################### L I N E  P L O T ###################################
 # here, we plot the different groups on a minute bin basis and compute lme analysis on the data
 
 # Load necessary libraries
-library(cowplot)
-library(ggsignif)
-library(ggplot2)
-library(colorspace)
-library(dplyr)
-library(openxlsx)
-library(tidyr)
-library(broom)
-library(readxl)
-library(rstatix)
-library(ggpubr)
-library(Hmisc)
-library(lme4)
+requiredPackages <- c("cowplot", "ggsignif", "ggplot2", "colorspace", "dplyr", 
+                      "openxlsx", "tidyr", "broom", "readxl", "rstatix", 
+                      "ggpubr", "Hmisc", "lme4", "broom.mixed")
 
-# Define the list of SUS animals
-sus_animals <- c("0001", "0004", "OR750", "OR751", "OR755", "OR762", "OR764", "OR770", "OR771", "OR106", "OR111", "OR112", "OR113", "OR120", "OR134")
+for (package in requiredPackages) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+  } else {
+    library(package, character.only = TRUE)
+  }
+}
 
-# Replace Group values with "SUS" for SUS animals, "RES" for SIS animals, and keep other groups as is
-data <- data %>%
-  mutate(Group = if_else(AnimalNum %in% sus_animals, "SUS",
-                         if_else(Group == "SIS", "RES", Group)))
-
+results_dir <- "C:/Users/topohl/Documents/test/cookie_analysis/"
 # Define group colors
 group_cols <- c("#1e3791", "#76A2E8", "#F79719")
+# Define the list of SUS animals
+sus_animals <- c(readLines(paste0("S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Analysis/sus_animals.csv")))
 
-cookie_data <- data %>% 
-  filter(Cookie == 'TRUE')
+# Add column named "Cookie" to the data frame, set it to true if DateTime is 17:00:00 and 18:00:00 and ConsecInactive "2"
+data_filtered <- data_filtered %>%
+  mutate(TimeOnly = format(DateTime, "%H:%M")) %>% # Extract only the time from DateTime
+  mutate(Cookie = ifelse(TimeOnly >= "17:00" & TimeOnly <= "17:45" & ConsecInactive == 2, "TRUE", "FALSE"))
+
+cookie_data <- data_filtered %>% 
+  filter(Cookie == 'TRUE') %>%
+  arrange(TimeOnly)  # Sort by time only
 
 # Calculate the time difference from the beginning of the DateTime dataset
 cookie_data$ElapsedMinutes <- as.numeric(cookie_data$DateTime - min(cookie_data$DateTime)) / 60
 
-# Now "ElapsedMinutes" column contains the number of elapsed minutes from the beginning of the DateTime dataset
-# Calculate the means and standard deviations of ActivityIndex for each group
-means_cookie <- cookie_data %>% 
-  group_by(Group, AnimalNum) %>% 
-  summarise(ActivityIndex = mean(ActivityIndex),
-            sd_ActivityIndex = sd(ActivityIndex))
+# Split the data into male and female datasets
+cookie_data_male <- cookie_data %>% filter(Sex == "m")
+cookie_data_female <- cookie_data %>% filter(Sex == "f")
 
-# Perform Shapiro-Wilk normality test for each group
+# Initialize a list to store LME results
+lme_results_list <- list()
 
-# Normality test for CON group
-con_norm_cookie <- shapiro.test(means_cookie$ActivityIndex[means_cookie$Group == "CON"])
+# Calculate overall y-axis limits
+y_min <- min(cookie_data_male$ActivityIndex, cookie_data_female$ActivityIndex)
+y_max <- max(cookie_data_male$ActivityIndex, cookie_data_female$ActivityIndex)
 
-# Normality test for RES group
-res_norm_cookie <- shapiro.test(means_cookie$ActivityIndex[means_cookie$Group == "RES"])
+# Function to perform analysis for a given dataset
+perform_analysis <- function(data) {
+  means_cookie <- data %>% 
+    group_by(Group, AnimalNum) %>% 
+    summarise(ActivityIndex = mean(ActivityIndex),
+              sd_ActivityIndex = sd(ActivityIndex),
+              .groups = 'drop') # Drop grouping
+  
+  # Fit linear mixed-effects models for the group using lmerTest for p-values
+  lme_model <- lmerTest::lmer(ActivityIndex ~ Group + ElapsedMinutes + (1 | AnimalNum), data = data)
+  
+  # Get the fitted values from the LME model
+  data$fitted_values <- fitted(lme_model)
 
-# Normality test for SUS group
-sus_norm_cookie <- shapiro.test(means_cookie$ActivityIndex[means_cookie$Group == "SUS"])
+  # Store the LME summary for exporting later
+  lme_summary <- summary(lme_model)
+  
+  # Get fixed effects summary and convert to dataframe
+  fixed_effects <- as.data.frame(lme_summary$coefficients)
+  
+  # Add Gender info
+  fixed_effects$Gender <- unique(data$Sex)  
+  
+  # Store results in the list
+  lme_results_list[[unique(data$Sex)]] <<- fixed_effects  # Use <<- to save it in the outer environment
 
-# Fit linear mixed-effects models for each group
-lme_model <- lmer(ActivityIndex ~ ElapsedMinutes + (1 | Group), data = cookie_data)
-
-# Get the fitted values from the LME model
-cookie_data$fitted_values <- fitted(lme_model)
-
-# Create plot for individual values
-p_indiv_cookie <- ggplot(cookie_data, aes(x = Group, y = ActivityIndex, color = Group)) +
+  # Create plot for individual values
+p_indiv_cookie <- ggplot(data, aes(x = Group, y = ActivityIndex, color = Group)) +
   geom_jitter(width = 0.2, height = 0, alpha = 0.5) + # Add jittered dots
   scale_fill_manual(values = group_cols) + # Set fill colors
-  scale_color_manual(values = group_cols) + # Set color scheme
-  scale_y_continuous(expand=c(0.1,0.1)) +
-  labs(title = "All Activity", x = "Group", y = "Activity Index [a.u.]") + # Add labels
+  scale_color_manual(values = group_cols) +
+  scale_y_continuous(limits = c(y_min, y_max), expand=c(0.1,0.1)) + # Use scale_y_continuous instead
+  labs(title = paste("All Activity -", unique(data$Sex)), x = "Group", y = "Activity Index [a.u.]") +
   theme_minimal_hgrid(12, rel_small = 1) +
   theme(plot.title = element_text(hjust = 0.5, face = "plain"),
         plot.subtitle = element_text(hjust = 0.5, face = "plain"),
         axis.text.x = element_text(angle = 0, vjust = 1, size = 11, hjust = 0.45),
         axis.title.x = element_blank(),
-        legend.position = "none") # Use a white and black theme
+        legend.position = "none") 
 
 # Create plot for means
 p_means_cookie <- ggplot(means_cookie, aes(x = Group, y = ActivityIndex, fill = Group, colour = Group)) +
-  geom_jitter(size=4, alpha=0.5, width=0.2, shape=16) + # Add dodged columns for mean values
+  geom_jitter(size=4, alpha=0.5, width=0.2, shape=16) + 
   geom_errorbar(aes(ymin = ActivityIndex - sd_ActivityIndex, ymax = ActivityIndex + sd_ActivityIndex), 
-                position = position_dodge(width = 0.9), width = 0.2) + # Add error bars
-  stat_summary(fun.min=function(z) {quantile(z,0.25)}, fun.max=function(z) {quantile(z,0.75)}, fun=median, color="black", size=1, shape=16) + # Add indication for mean and error bars
-  scale_fill_manual(values = group_cols) + # Set fill colors
+                position = position_dodge(width = 0.9), width = 0.2) +
+  stat_summary(fun.min=function(z) {quantile(z,0.25)}, fun.max=function(z) {quantile(z,0.75)}, fun=median, color="black", size=1, shape=16) +
+  scale_fill_manual(values = group_cols) +
   scale_color_manual(values = group_cols) +
-  scale_y_continuous(expand=c(0.1,0.1)) +
-  labs(title = "Individual Activity", x = "Group", y = "Activity Index [a.u.]") + # Add labels
+  scale_y_continuous(limits = c(y_min, y_max), expand=c(0.1,0.1)) + # Use scale_y_continuous instead
+  labs(title = paste("Individual Activity -", unique(data$Sex)), x = "Group", y = "Activity Index [a.u.]") +
   theme_minimal_hgrid(12, rel_small = 1) +
   theme(plot.title = element_text(hjust = 0.5, face = "plain"),
         plot.subtitle = element_text(hjust = 0.5, face = "plain"),
         axis.text.x = element_text(angle = 0, vjust = 1, size = 11, hjust = 0.45),
         axis.title.x = element_blank(),
-        legend.position = "none") # Use a white and black theme
+        legend.position = "none") 
+  
+  # Arrange two plots side by side
+  combined_plot <- plot_grid(p_indiv_cookie, p_means_cookie, ncol = 2, align = "v", labels = c("A", "B"))
+  
+  # Create cookie plot with adjusted summary functions
+  cookie_plot <- ggplot(data, aes(x = TimeOnly, y = ActivityIndex, group = Group, color = Group)) +
+    stat_summary(aes(group = Group), fun = mean, geom = "line", size = 1) + # Plot the mean as lines
+    stat_summary(aes(fill = Group), fun = mean,
+                 fun.min = function(x) mean(x) - sd(x), 
+                 fun.max = function(x) mean(x) + sd(x), 
+                 geom = "ribbon", 
+                 alpha = 0.2, colour = NA) + # Add ribbons for SD
+    scale_color_manual(name = NULL, values = group_cols) + 
+    scale_fill_manual(name = NULL, values = group_cols) +
+    theme_minimal_hgrid(12, rel_small = 1) +
+    labs(title = bquote(~bold(.(paste("Cookie Challenge -", unique(data$Sex))))),
+         y = "Activity [a.u.]", x = "Time", caption = "") + # Update x-axis label to "Time"
+    theme(plot.title = element_text(hjust = 0.5, face = "plain"),
+          plot.subtitle = element_text(hjust = 0.5, face = "plain"),
+          legend.position = "top",
+          legend.justification = "right",
+          legend.text = element_text(size = 9),
+          legend.box.spacing = unit(1, "pt"),
+          axis.text.x = element_text(vjust = 1, size = 11, hjust = 0.5),
+          axis.title.x = element_blank())
 
-# Arrange two plots side by side
-plot_grid(p_indiv_cookie, p_means_cookie, ncol = 2, align = "v", labels = c("A", "B"))
+  # Save the plots
+  ggsave(filename = paste0(results_dir, "cookie_plot_", unique(data$Sex), ".svg"), plot = cookie_plot, width = 8, height = 6, dpi = 300)
+  ggsave(filename = paste0(results_dir, "combined_cookie_plot_", unique(data$Sex), ".svg"), plot = combined_plot, width = 8, height = 6, dpi = 300)
+}
 
-cookie_plot <- ggplot(cookie_data, aes(x = DateTime, y = ActivityIndex, group = Group, color = Group)) +
-  geom_path(stat = "summary") +
-  stat_summary(aes(fill = Group), fun = mean,
-               fun.min = function(x) mean(x) - sd(x), 
-               fun.max = function(x) mean(x) + sd(x), 
-               geom = "ribbon", 
-               alpha = 0.2, colour = NA) +
-  scale_color_manual(name = NULL, values = group_cols) + 
-  scale_fill_manual(name = NULL, values = group_cols) +
-  theme_minimal_hgrid(12, rel_small = 1) +
-  labs(title = bquote(~bold(.(paste("Cookie Challenge")))),
-       y = "Activity [a.u.]", x = "Day/Night", caption = "") +
-  theme(plot.title = element_text(hjust = 0.5, face = "plain"),
-        plot.subtitle = element_text(hjust = 0.5, face = "plain"),
-        legend.position = "top",
-        legend.justification = "right",
-        legend.text = element_text(size = 9),
-        legend.box.spacing = unit(1, "pt"),
-        axis.text.x = element_text(vjust = 1, size = 11, hjust = 0.5),
-        axis.title.x = element_blank())
+# Perform analysis for males and females separately
+perform_analysis(cookie_data_male)
+perform_analysis(cookie_data_female)
 
-cookie_plot
+# Combine and save the LME results to CSV
+lme_results_combined <- do.call(rbind, lme_results_list)
+write.csv(lme_results_combined, file = paste0(results_dir, "lme_results.csv"), row.names = TRUE)
